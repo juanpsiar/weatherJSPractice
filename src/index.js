5ea79d0fd97fad7a3e6b846d25e1220f80f51cd4
JuanPSigcho <juanpsigcho@gmail.com>
2021-09-06 16:11:47 -0500

    Integrate changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './style/main.scss';
+
+var city = document.getElementById('city');
+
+getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    return 'Geolocation is not supported by this browser.';
+  }
+};
+
+const setData = (dataCoord) => {
+  cords = dataCoord;
+  console.log('cords', cords);
+};
+
+const showPosition = (position) => {
+  let coordinates = {
+    Latitude: position.coords.latitude,
+    Longitude: position.coords.longitude,
+  };
+  city.innerHTML = ` Latitude:${position.coords.latitude},
+  Longitude: ${position.coords.longitude}`;
+
+  setData(coordinates);
+};
+
+console.log('getLocation', getLocation());
